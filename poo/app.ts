8190e3b0f18aa90efbc2e724eb7e7784954ffe58
Lucas Michels <lucmichels94@gmail.com>
2025-07-30 22:57:47 -0300

    Herança

--- poo/app.ts
+++ poo/app.ts
@@ -1,19 +1,39 @@
-class User {
-    name: string = 'John Doe';
-    age: number = 30;
+// Dio Banking
+// name, accountNumber, balance
+// deposit, withdraw, getBalance
 
-    constructor(name: string, age: number) {
+abstract class Account {
+    name: string;
+    accountNumber: string;
+    balance: number;
+
+    constructor(name: string, accountNumber: string) {
         this.name = name;
-        this.age = age;
+        this.accountNumber = accountNumber;
+        this.balance = 0
     }
 
-    showName() {
-        console.log(this.name);
+    deposit = (amount: number): void => {
+        console.log(`Depositing ${amount} to account ${this.accountNumber}`);
+    }
+
+    withdraw = (amount: number): void => {
+        console.log(`Withdrawing ${amount} from account ${this.accountNumber}`);
+    }
+
+    getBalance = (): void => {
+        console.log(`Current balance is ${this.balance}`);
     }
 }
 
-const user = new User("John Doe", 30);
-user.showName(); // Output: John Doe
+class UserAccount extends Account {
+    docId: string;
 
-const otherUser = new User("Jane Smith", 25);
-otherUser.showName(); // Output: Jane Smith
+    constructor(name:string, accountNumber:string, docId: string) {
+        super(name, accountNumber)
+        this.docId = docId;
+    }
+}
+
+const userAccount: UserAccount = new UserAccount("John Doe", "0001", "123456789");
+console.log(userAccount)
